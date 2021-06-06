68d96d562180fb9d63f829b17acdd9758356366b
dicarlomtz <dicarlomtz@gmail.com>
2021-06-05 22:42:27 -0600

    Agregando dormir

--- main.c
+++ main.c
@@ -3,17 +3,26 @@
 #include <unistd.h>
 #include <stdbool.h>
 #include <pthread.h>
+#include <semaphore.h>
+
+//Referencias:
+//Notas, ejemplos y laboratorios de clase
+//https://www.youtube.com/watch?v=YC61729PThw
 
 #define CAMPOS_ESPERA 3
-long espera[CAMPOS_ESPERA] = {0, 0, 0};
-long idEstAten = 0;
-int cantEst = 0;
-int timeA = 0;
+long espera[CAMPOS_ESPERA] = {0, 0, 0}; //Lista de espera (sillas)
+long idEstAten = 0;                     //Estudiante que actualmente se está atendiendo (nunca es 0)
+int cantEst = 0;                        //Cantidad de estudiantes
+int timeA = 0;                          //Tiempo de atención (horario de atencion)
+bool siestaBandera = false;
+sem_t siesta;
 
-pthread_mutex_t esperaMutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t atencionMutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t vecesMutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t esperaMutex = PTHREAD_MUTEX_INITIALIZER;   //Controla el acceso a la lista de espera
+pthread_mutex_t atencionMutex = PTHREAD_MUTEX_INITIALIZER; //Controla el tiempo de atención para sincronizar
+pthread_mutex_t vecesMutex = PTHREAD_MUTEX_INITIALIZER;    //Controla el acceso a la disminución de veces que se ejecuta (horario de atencion)
 
+//Verifica si hay estudiantes esperando
+//Nunca un id es 0, por lo que no existe un estudiante con ese id
 bool comprobarEspera()
 {
     if (espera[0] != 0)
@@ -21,6 +30,8 @@ bool comprobarEspera()
     return false;
 }
 
+//Retorna el estudiante que esta al inicio y corre los campos
+//Retorna el id del estudiante (nunca un id es 0)
 int obtenerEstAtend()
 {
     pthread_mutex_lock(&esperaMutex);
@@ -32,28 +43,53 @@ int obtenerEstAtend()
     return tmp;
 }
 
-bool buscAtenc(long idEst)
+//Se coloca al estudiante en la primer posicion, puesto que este despierta al asistente.
+bool despertar(long idEst)
 {
     pthread_mutex_lock(&esperaMutex);
-    for (int i = 0; i < 3; i++)
-    {
-        if (espera[i] == 0)
-        {
-            espera[i] = idEst;
-            pthread_mutex_unlock(&esperaMutex);
-            return true;
-        }
-    }
+    espera[0] = idEst;
     pthread_mutex_unlock(&esperaMutex);
-    return false;
+    sem_wait(&siesta);
+    siestaBandera = false;
+    printf("Estudiante %ld despertando a asistente...\n", idEst);
+    sem_post(&siesta);
+    return true;
 }
 
+//Un estudiante busca la atención
+//Se coloca en espera en la posición más cercana si hay espacio
+//Si el asistente está duermiendo, lo despierta
+bool buscAtenc(long idEst)
+{
+    if (!siestaBandera)
+    {
+        pthread_mutex_lock(&esperaMutex);
+        for (int i = 0; i < 3; i++)
+        {
+            if (espera[i] == 0)
+            {
+                espera[i] = idEst;
+                pthread_mutex_unlock(&esperaMutex);
+                return true;
+            }
+        }
+        pthread_mutex_unlock(&esperaMutex);
+        return false;
+    }
+    else
+    {
+        return despertar(idEst);
+    }
+}
+
+//Retorna el tiempo que será atendido el estudiante (random)
 int tiemAten()
 {
     srand(time(NULL));
     return 5 + rand() % 10;
 }
 
+//Rifa quien sera el proximo en ser atendido (random id de estudiantes)
 long rifaAten()
 {
     srand(time(NULL));
@@ -64,18 +100,27 @@ void *thread_asistente()
 {
     while (timeA > 0)
     {
-        if (comprobarEspera())
+        if (comprobarEspera()) //Si hay un estudiante esperando
         {
+
             pthread_mutex_lock(&atencionMutex);
-            idEstAten = obtenerEstAtend();
+            idEstAten = obtenerEstAtend(); //Lo obtiene y se atiende
             printf("Asistente inicia atencion a estudiante %ld...\n", idEstAten);
             sleep(tiemAten());
-            pthread_mutex_unlock(&atencionMutex);
             printf("Asistente finaliza atencion a estudiante %ld...\n", idEstAten);
+            pthread_mutex_unlock(&atencionMutex);
         }
         else
         {
-            //duerme
+            //Pone a dormir al asistente
+            sem_wait(&siesta);
+            siestaBandera = true;
+            printf("Asistente va a tomar una siesta...\n");
+            sem_post(&siesta);
+            while (siestaBandera && timeA > 0)
+            {
+                sleep(0.1);
+            }
         }
     }
     printf("El horario de atencion ha finalizado...\n");
@@ -89,19 +134,25 @@ void *thread_estudiante(void *idPtr)
     {
 
         printf("Estudiante %ld programando...\n", id);
-
-        if (rifaAten() == id)
+        sleep(2);
+        if (rifaAten() == id) //Se realiza la rifa para buscar atencion
         {
             printf("Estudiante %ld buscando atencion...\n", id);
-            if (buscAtenc(id))
+            if (buscAtenc(id)) //Se pone en cola para la atencion si hay campo
             {
+                //Puede que en el momento que se ponga en cola no esté siendo atendido, espera a serlo.
+                //Por lo que mientras que el estudiante que se esté atendiendo sea diferente de el(idEstAten)
+                //Va a esperar siempre y cuando haya tiempo para atenderse
+
                 while (idEstAten != id && timeA > 0)
                 {
                     printf("Estudiante %ld esperando atencion...\n", id);
                     sleep(5);
                 }
-                pthread_mutex_lock(&atencionMutex);
 
+                pthread_mutex_lock(&atencionMutex);
+                //Cuando ya está haciedo atendido termina el bucle, pero debe esperar que el asistente
+                //libere el espacio de atención
                 pthread_mutex_unlock(&atencionMutex);
             }
             else
@@ -111,7 +162,7 @@ void *thread_estudiante(void *idPtr)
         }
         sleep(1.5);
         pthread_mutex_lock(&vecesMutex);
-        timeA--;
+        timeA--; //Se disminuye el tiempo de atencion (horario)
         pthread_mutex_unlock(&vecesMutex);
     }
 }
@@ -119,12 +170,13 @@ void *thread_estudiante(void *idPtr)
 int main(int argc, char *argv[])
 {
     cantEst = atoi(argv[1]);
-    timeA = cantEst * 6;
+    timeA = cantEst * 6; //Para tener un tiempo de atención aceptable, se hace la operacion
     pthread_t asistente;
     pthread_t estudiante[cantEst];
 
     int rc;
 
+    sem_init(&siesta, 0, 1);
     rc = pthread_create(&asistente, NULL, thread_asistente, NULL);
 
     if (rc)
@@ -136,7 +188,6 @@ int main(int argc, char *argv[])
     for (long i = 0; i < cantEst; i++)
     {
         rc = pthread_create(&estudiante[i], NULL, thread_estudiante, (void *)i + 1);
-
         if (rc)
         {
             fprintf(stderr, "ERROR: no se pudo crear el thread estudiante\n");
